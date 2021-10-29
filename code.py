735ed50ebe73ada8255183f80454750f08bebad1
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2021-10-29 04:05:30 -0400

    Update code.py

--- code.py
+++ code.py
@@ -16,18 +16,8 @@ encoder = rotaryio.IncrementalEncoder(board.D10, board.D9)
 last_position = encoder.position
 
 pixel_pin = board.D2
-num_pixels = 4
-
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False)
-
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            rc_index = (i * 256 // num_pixels) + j
-            pixels[i] = colorwheel(rc_index & 255)
-        pixels.show()
-        time.sleep(wait)
+LED_index = [3, 1, 0, 2] # Up Down Left Right
+pixels = neopixel.NeoPixel(pixel_pin, len(LED_index), brightness=1, auto_write=False)
 
 BLACK = (0,0,0)
 RED = (255, 0, 0)
@@ -38,9 +28,9 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 
 keySwitchLEDTimeout = [0,0,0,0]
-KeySwitchPins = [board.D7, board.D3, board.D1, board.D4] # Up Down Left Right
-LED_index = [3, 1, 0, 2] # Up Down Left Right\
+KeySwitchPins = [board.D8, board.D3, board.D1, board.D4] # Up Down Left Right
 KeySwitchIOs = []
+
 for key in KeySwitchPins:
     thisSwitch = digitalio.DigitalInOut(key)
     thisSwitch.pull = digitalio.Pull.UP
@@ -48,12 +38,14 @@ for key in KeySwitchPins:
 
 while True:
     time.sleep(0.1)
+    
     for i in range(len(KeySwitchIOs)):
         if KeySwitchIOs[i].value is False:
             pixels[LED_index[i]] = BLUE
             pixels.show()
         else:
             pixels[LED_index[i]] = BLACK
+
 
     current_position = encoder.position
     position_change = current_position - last_position
